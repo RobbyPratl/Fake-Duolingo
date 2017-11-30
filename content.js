2617f7fbb136260110e33967156f0f851c8c4bc9
RobbyPratl <34143233+RobbyPratl@users.noreply.github.com>
2017-11-30 16:57:29 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-document.title = "Duolingo";
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://www.duolingo.com/images/favicon.ico?v=3';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
+setInterval(function(){ 
+    document.title = 'Duolingo';   
+}, 5000);
